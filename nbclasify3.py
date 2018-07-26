f606edeb22feb2e45fb539c47c3489c120a7c37c
Sachin Malhotra <sachinma@usc.edu>
2018-07-26 01:25:25 -0700

    Added comments

--- nbclasify3.py
+++ nbclasify3.py
@@ -11,9 +11,13 @@ class Classify:
         self.unique_word_count = 0
         self.variables = {}
 
+    # Load the model's learned parameters
     def load(self):
+        # literal_eval is used to convert string representation of Python's dictionary to the actual object
+        # This is one use case of it.
         from ast import literal_eval
         saved = open(self.saved_model, "r")
+        # Load all the saved values
         self.conditional_probabilities = literal_eval(saved.readline().strip())
         self.prior_probabilities = literal_eval(saved.readline().strip())
         self.unique_word_count = int(saved.readline().strip())
@@ -25,21 +29,32 @@ class Classify:
             return self.conditional_probabilities[class_initial][word]
         return math.log(1 / (self.unique_word_count + self.variables[class_initial]))
 
+    # Classify individual review
     def classify_review(self, review):
         true_prob, fake_prob, pos_prob, neg_prob = self.prior_probabilities['t'], self.prior_probabilities['f']\
             , self.prior_probabilities['p'], self.prior_probabilities['n']
+
+        # Remove punctuation and stop words from the review
         review = util3.remove_stop_words(util3.remove_punctuation(review))
         identifier, *review = review.split()
 
+        # Note, here we add the conditional probabilities of every word belonging to each of the classes.
+        # We are able to add here because we took log of the probabilities earlier during training.
+        # If we had taken probabilities by themselves, we would have had to multiply them.
+        # P(True | Review) = P(True) * P(Review | True)
+        # P(Review | True) = P(W1 | True) * P(W2 | True) * P(W3 | True) * .... P(Wn | True)
+        # So, for log probabilities, we can add instead of multiplication
         for word in review:
             true_prob += self.get_conditional_probability(word, 't')
             fake_prob += self.get_conditional_probability(word, 'f')
             pos_prob += self.get_conditional_probability(word, 'p')
             neg_prob += self.get_conditional_probability(word, 'n')
 
+        
         return identifier, "True" if true_prob > fake_prob else "Fake", \
                 "Pos" if pos_prob > neg_prob else "Neg"
 
+    # Classify all the reviews in a given test file.
     def classify_test_file(self):
         output_file = open(self.output_file, "w")
         with open(self.test_file) as f:
--- nblearn3.py
+++ nblearn3.py
@@ -7,18 +7,29 @@ class NaiveBayesClassifier:
         self.model_file = "nbmodel.txt"
 
         """ Dictionaries required for the model. """
+
+        # A dictionary that keeps a track of the number of times a word occurred in a negative review
         self.negative_word_count = {}
+
+        # A dictionary that keeps a track of the number of times a word occurred in a positive review
         self.positive_word_count = {}
+
+        # A dictionary that keeps a track of the number of times a word occurred in a true review
         self.true_word_count = {}
+
+        # A dictionary that keeps a track of the number of times a word occurred in a fake review
         self.fake_word_count = {}
+
+        # A collective dictionary for simplified access and updates of the above defined dictionaries.
         self.dictionaries = {"t" : self.true_word_count, "f" : self.fake_word_count,
                              "p" : self.positive_word_count, "n" : self.negative_word_count}
 
-        """ Variables required for the model """
+        # A dictionary to keep track of counts of each class
         self.variables = {"t" : 0, "f" : 0,
                           "p" : 0, "n" : 0}
 
 
+        # Prior probabilities to be used by the Naïve Bayes classifier.
         self.prior_probabilities = {"t" : 0, "f" : 0,
                                      "p" : 0, "n" : 0}
         self.unique_words = set()
@@ -30,6 +41,10 @@ class NaiveBayesClassifier:
         self.dictionaries[review[0].lower()][word] = self.dictionaries[review[0].lower()].get(word, 0) + 1
 
     """ Laplace smoothing included for unknown combinations. """
+    # Laplace smoothing probability = count + 1 / N + |V|
+    # count is for the word
+    # N is the number of words in that class
+    # |V| is the vocabulary size i.e. the number of unique words in the corpus
     def conditional_probabilities(self, type, word, div):
         if word not in self.dictionaries[type]:
             self.dictionaries[type][word] = 1 / div
@@ -40,25 +55,38 @@ class NaiveBayesClassifier:
 
     def train(self):
         with open(self.file) as f:
+            # For every review, do this processing
             for line in f:
+                # Remove punctuations and stop words from the reviews
                 line = util3.remove_stop_words(util3.remove_punctuation(line))
+
+                # Obtain the review's classes, it's identifier and the review itself
                 identifier, true_or_fake, pos_or_neg, *review = line.strip().split()
+
+                # Update prior probability counts for individual classes for this review
                 self.update_priors(true_or_fake)
                 self.update_priors(pos_or_neg)
 
+                # For every word in this review, update it's corresponding occurrence count
                 for word in review:
                     self.update_word_count(word, true_or_fake)
                     self.update_word_count(word, pos_or_neg)
+                    # Also, add this word to a set of unique words maintained over the entire corpus.
                     self.unique_words.add(word)
 
-
+        # Once we have processed the entire corpus, calculate prior probabilities
+        # P(Fake) = count(Fake) / (count (Fake) + count(True))
+        # log(P(Fake)) = log probabilities make calculations easier further on in the implementation.
         self.prior_probabilities["f"] = math.log(self.variables["f"] / (self.variables["f"] + self.variables["t"]))
         self.prior_probabilities["t"] = math.log(self.variables["t"] / (self.variables["f"] + self.variables["t"]))
         self.prior_probabilities["n"] = math.log(self.variables["n"] / (self.variables["n"] + self.variables["p"]))
         self.prior_probabilities["p"] = math.log(self.variables["p"] / (self.variables["n"] + self.variables["p"]))
 
+        # A tuple containing total number of words in each of the classes
         divs = sum(self.dictionaries['p'].values()), sum(self.dictionaries['n'].values()),\
         sum(self.dictionaries['f'].values()), sum(self.dictionaries['t'].values())
+
+        # Calculate laplace smoothed probabilities for every word in the corpus for every class
         for word in self.unique_words:
             self.conditional_probabilities('t', word, divs[3] + len(self.unique_words))
             self.conditional_probabilities('f', word, divs[2] + len(self.unique_words))
@@ -70,6 +98,7 @@ class NaiveBayesClassifier:
         self.variables['p'] = divs[0]
         self.variables['n'] = divs[1]
 
+    # Save the model parameters.
     def save_model(self):
         with open(self.model_file, "w") as f:
             f.write(str(self.dictionaries))
